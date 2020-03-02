5cbe08f6fc12ae87d0d29f18f22ca425d675b19f
grvahuja205 <grvahuja205@gmail.com>
2020-03-03 00:05:07 +0530

    Updated the basic data insertion and deletion scripts according to new models of screen being added

--- app/migrations/create_db.py
+++ app/migrations/create_db.py
@@ -1,129 +1,267 @@
-from app.models import db, Theater, SeatingsRow
+from app.models import db, Screen, SeatingsRow, Theater
 
 THEATER_SEATING_DATA = [
     {
         "theater_name" : "Inox",
         "theater_slug": "ionx",
-        "seating":[
+        "screens":[
             {
-                "name": "A",
-                "slug":"a",
-                "seat_nos" :[
-                    (1, True),
-                    (2, False),
-                    (3, False),
-                    (4, True),
-                    (5, True),
-                    (6, False),
-                    (7, False),
-                    (8, True)
-                ],
-            },
-            {
-                "name": "B",
-                "slug":"b",
-                "seat_nos": [
-                    (1, False),
-                    (2, True),
-                    (3, True),
-                    (4, False),
-                    (5, False),
-                    (6, True),
-                    (7, True),
-                    (8, False)
-                ],
-            },
-            {
-                "name": "C",
-                "slug": "c",
-                "seat_nos": [
-                    (1, False),
-                    (2, False),
-                    (3, False),
-                    (4, False),
-                    (5, False),
-                    (6, False),
-                    (7, True),
-                    (8, False)
-                ],
-            },
-            {
-                "name": "D",
-                "slug": "d",
-                "seat_nos":[
-                    (1, True),
-                    (2, False),
-                    (3, False),
-                    (4, False),
-                    (5, False),
-                    (6, False),
-                    (7, False),
-                    (8, True)
-                ]
+                "screen_name":"Screen 1",
+                "screen_slug":"screen_1",
+                "seatings":[
+                    {
+                        "name": "A",
+                        "slug":"a",
+                        "seat_nos" :[
+                            (1, True),
+                            (2, False),
+                            (3, False),
+                            (4, True),
+                            (5, True),
+                            (6, False),
+                            (7, False),
+                            (8, True)
+                        ],
+                    },
+                    {
+                        "name": "B",
+                        "slug":"b",
+                        "seat_nos": [
+                            (1, False),
+                            (2, True),
+                            (3, True),
+                            (4, False),
+                            (5, False),
+                            (6, True),
+                            (7, True),
+                            (8, False)
+                        ],
+                    },
+                    {
+                        "name": "C",
+                        "slug": "c",
+                        "seat_nos": [
+                            (1, False),
+                            (2, False),
+                            (3, False),
+                            (4, False),
+                            (5, False),
+                            (6, False),
+                            (7, True),
+                            (8, False)
+                        ],
+                    },
+                    {
+                        "name": "D",
+                        "slug": "d",
+                        "seat_nos":[
+                            (1, True),
+                            (2, False),
+                            (3, False),
+                            (4, False),
+                            (5, False),
+                            (6, False),
+                            (7, False),
+                            (8, True)
+                        ]
 
+                    }
+                ]
+            },
+            {
+                "screen_name": "Screen 2",
+                "screen_slug": "screen_2",
+                "seatings": [
+                    {
+                        "name": "A",
+                        "slug": "a",
+                        "seat_nos": [
+                            (1, True),
+                            (2, False),
+                            (3, False),
+                            (4, True),
+                            (5, True),
+                            (6, False),
+                            (7, False),
+                            (8, True)
+                        ],
+                    },
+                    {
+                        "name": "B",
+                        "slug": "b",
+                        "seat_nos": [
+                            (1, False),
+                            (2, True),
+                            (3, True),
+                            (4, False),
+                            (5, False),
+                            (6, True),
+                            (7, True),
+                            (8, False)
+                        ],
+                    },
+                    {
+                        "name": "C",
+                        "slug": "c",
+                        "seat_nos": [
+                            (1, False),
+                            (2, False),
+                            (3, False),
+                            (4, False),
+                            (5, False),
+                            (6, False),
+                            (7, True),
+                            (8, False)
+                        ],
+                    },
+                    {
+                        "name": "D",
+                        "slug": "d",
+                        "seat_nos": [
+                            (1, True),
+                            (2, False),
+                            (3, False),
+                            (4, False),
+                            (5, False),
+                            (6, False),
+                            (7, False),
+                            (8, True)
+                        ]
+
+                    }
+                ]
             }
         ]
     },
     {
         "theater_name" : "Pvr",
         "theater_slug": "pvr",
-        "seating":[
+        "screens":[
             {
-                "name": "A",
-                "slug": "a",
-                "seat_nos" :[
-                    (1, False),
-                    (2, False),
-                    (3, False),
-                    (4, False),
-                    (5, False),
-                    (6, False),
-                    (7, True),
-                    (8, False)
-                ],
-            },
-            {
-                "name": "B",
-                "slug": "b",
-                "seat_nos": [
-                    (1, True),
-                    (2, False),
-                    (3, False),
-                    (4, False),
-                    (5, False),
-                    (6, False),
-                    (7, False),
-                    (8, True)
-                ],
-            },
-            {
-                "name": "C",
-                "slug": "c",
-                "seat_nos": [
-                    (1, False),
-                    (2, True),
-                    (3, True),
-                    (4, False),
-                    (5, False),
-                    (6, True),
-                    (7, True),
-                    (8, False)
-                ],
-            },
-            {
-                "name": "D",
-                "slug": "d",
-                "seat_nos":[
-                    (1, True),
-                    (2, False),
-                    (3, False),
-                    (4, True),
-                    (5, True),
-                    (6, False),
-                    (7, False),
-                    (8, True)
-                ]
+                "screen_name":"Screen 1",
+                "screen_slug":"screen_1",
+                "seatings":[
+                    {
+                        "name": "A",
+                        "slug": "a",
+                        "seat_nos" :[
+                            (1, False),
+                            (2, False),
+                            (3, False),
+                            (4, False),
+                            (5, False),
+                            (6, False),
+                            (7, True),
+                            (8, False)
+                        ],
+                    },
+                    {
+                        "name": "B",
+                        "slug": "b",
+                        "seat_nos": [
+                            (1, True),
+                            (2, False),
+                            (3, False),
+                            (4, False),
+                            (5, False),
+                            (6, False),
+                            (7, False),
+                            (8, True)
+                        ],
+                    },
+                    {
+                        "name": "C",
+                        "slug": "c",
+                        "seat_nos": [
+                            (1, False),
+                            (2, True),
+                            (3, True),
+                            (4, False),
+                            (5, False),
+                            (6, True),
+                            (7, True),
+                            (8, False)
+                        ],
+                    },
+                    {
+                        "name": "D",
+                        "slug": "d",
+                        "seat_nos":[
+                            (1, True),
+                            (2, False),
+                            (3, False),
+                            (4, True),
+                            (5, True),
+                            (6, False),
+                            (7, False),
+                            (8, True)
+                        ]
 
+                    }
+                ]
+            },
+            {
+                "screen_name": "Screen 2",
+                "screen_slug": "screen_2",
+                "seatings": [
+                    {
+                        "name": "A",
+                        "slug": "a",
+                        "seat_nos": [
+                            (1, False),
+                            (2, False),
+                            (3, False),
+                            (4, False),
+                            (5, False),
+                            (6, False),
+                            (7, True),
+                            (8, False)
+                        ],
+                    },
+                    {
+                        "name": "B",
+                        "slug": "b",
+                        "seat_nos": [
+                            (1, True),
+                            (2, False),
+                            (3, False),
+                            (4, False),
+                            (5, False),
+                            (6, False),
+                            (7, False),
+                            (8, True)
+                        ],
+                    },
+                    {
+                        "name": "C",
+                        "slug": "c",
+                        "seat_nos": [
+                            (1, False),
+                            (2, True),
+                            (3, True),
+                            (4, False),
+                            (5, False),
+                            (6, True),
+                            (7, True),
+                            (8, False)
+                        ],
+                    },
+                    {
+                        "name": "D",
+                        "slug": "d",
+                        "seat_nos": [
+                            (1, True),
+                            (2, False),
+                            (3, False),
+                            (4, True),
+                            (5, True),
+                            (6, False),
+                            (7, False),
+                            (8, True)
+                        ]
+
+                    }
+                ]
             }
         ]
     }
@@ -135,16 +273,23 @@ def create_movie_db_data():
                           slug=theater_seating_details.get('theater_slug'))
         db.session.add(theater)
         db.session.flush()
-        for seating_details in theater_seating_details.get('seating'):
+        for screen_details in theater_seating_details.get('screens'):
 
-            for row_number in seating_details.get('seat_nos'):
-                row = SeatingsRow(
-                    theater_id=theater.id,
-                    row_name=seating_details.get("name"),
-                    slug=seating_details.get("slug")
-                )
-                row.row_number = row_number[0]
-                row.is_aisle = row_number[1]
-                db.session.add(row)
+            screen = Screen(name=screen_details.get("screen_name"),
+                            slug=screen_details.get("screen_slug"),
+                            theater_id=theater.id)
+            db.session.add(screen)
+            db.session.flush()
+
+            for seating_details in screen_details.get('seatings'):
+                for row_details in seating_details.get('seat_nos'):
+                    row = SeatingsRow(
+                        screen_id=screen.id,
+                        row_name=seating_details.get("name"),
+                        slug=seating_details.get("slug")
+                    )
+                    row.row_number = row_details[0]
+                    row.is_aisle = row_details[1]
+                    db.session.add(row)
 
                 db.session.commit()
--- app/migrations/delete_basic_db.py
+++ app/migrations/delete_basic_db.py
@@ -1,9 +1,11 @@
-from app.models import db, Theater as TheaterModel, SeatingsRow as SeatingsRowModel
+from app.models import db, Theater as TheaterModel, \
+    SeatingsRow as SeatingsRowModel, Screen as ScreenModel
 
 
 def delete_data():
     try:
         db.session.query(SeatingsRowModel).delete()
+        db.session.query(ScreenModel).delete()
         db.session.query(TheaterModel).delete()
     except Exception as e:
         print(str(e))
